21b00503d65135088460ebb4908f72ac4e7c21da
aleja <aleja@192.168.0.106>
2020-11-27 12:59:09 +0100

    Ejercicio 9. No se muestra la salida correctamente en los ficheros respectivos

--- PSP_AlejandroEscudero/src/PSP/UD01/programacionMultiproceso/Ejercicio08.java
+++ PSP_AlejandroEscudero/src/PSP/UD01/programacionMultiproceso/Ejercicio08.java
@@ -4,32 +4,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 /**
- * 1) Crea una clase Java cuyo método main:
- * 1.1)Cree un objeto ProcessBuilder a través del cual consultar las
- * variables de entorno.
- * 1.1) Cree un segundo objeto ProcessBuilder para lanzar un proceso que
- * muestre la ayuda del sistema para el comando ls (ls --help). Antes de
- * lanzarlo, deben listarse los argumentos de la llamada
- * @author Alejandro Escudero Sánchez
- * 2º DAM
+ * 1) Crea una clase Java cuyo método main: 1.1)Cree un objeto ProcessBuilder a
+ * través del cual consultar las variables de entorno. 1.1) Cree un segundo
+ * objeto ProcessBuilder para lanzar un proceso que muestre la ayuda del sistema
+ * para el comando ls (ls --help). Antes de lanzarlo, deben listarse los
+ * argumentos de la llamada
+ * 
+ * @author Alejandro Escudero Sánchez 2º DAM
  *
  */
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
-		//Apartadp 1.1
-		ProcessBuilder procEnvironment = new ProcessBuilder();
+		// Apartadp 1.1
+		ProcessBuilder procEnvironment = new ProcessBuilder(); // para consultar las variables de entorno
+
 		Map<String, String> variables = procEnvironment.environment();
 		System.out.println(variables);
 		List<String> listaComandos = new ArrayList<String>();
 		listaComandos.add("CMD");
 		listaComandos.add("/C");
 		listaComandos.add("DIRR");
-		
+		listaComandos.add("ls"); // 8.2
+		listaComandos.add("--help");
 		ProcessBuilder procesBuild = new ProcessBuilder(listaComandos);
 		System.out.println(procesBuild.command());
-		
+		// 8.2
+		ProcessBuilder procAyuda = new ProcessBuilder(listaComandos);
+		System.out.println(procAyuda.command());
+
 		try {
 			procesBuild.start();
 		} catch (IOException e) {
---
+++ PSP_AlejandroEscudero/src/PSP/UD01/programacionMultiproceso/Ejercicio09.java
@@ -0,0 +1,63 @@
+package PSP.UD01.programacionMultiproceso;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Realizar una clase Java cuyo método main cree un ProcessBuilder con un List
+ * de String realizando un listado del directorio src. Las salidas estándar y de
+ * error deben ser redirigidas a un fichero cuyo nombre sea
+ * salida_ejercicio9.txt y log_ejercicio9.txt respectivamente. Además, por
+ * pantalla debe indicarnos un mensaje sobre qué fichero consultar en función de
+ * si la ejecución del proceso ha sido o no errónea
+ * 
+ * @author Alejandro Escudero Sánchez 2º DAM
+ *
+ */
+public class Ejercicio09 {
+
+	public static void main(String[] args) {
+		Process proceso = null;
+		int estado = -1;
+		String comandoLinux = "ls -help";
+		String comandoWindows = "CMD /C DIR C:\\\\Users\\\\aleja\\\\git\\\\PSP_AlejandroEscudero\\\\PSP_AlejandroEscudero\\\\ficheros";
+		String comando = comandoWindows;
+		List<String> listaComandos = new ArrayList<String>();
+		listaComandos.add("CMD");
+		listaComandos.add("DIR");
+		listaComandos.add("/C");
+		listaComandos.add("C:\\Users\\aleja\\git\\PSP_AlejandroEscudero\\PSP_AlejandroEscudero\\ficheros");
+
+		// Creamos el procesBuilder de la lista de comandos
+		ProcessBuilder procesBuild = new ProcessBuilder(listaComandos);
+
+		// Indicamos los respectivos ficheros de salida
+		File ficheroSalida = new File(Ejercicio03.getCadenaFichero("salida_ejercicio9.txt"));
+		File ficheroSalidaError = new File(Ejercicio03.getCadenaFichero("log_ejercicio9.txt"));
+		procesBuild.redirectOutput(ficheroSalidaError);
+		procesBuild.redirectError(ficheroSalida);
+
+		try {
+
+			proceso = procesBuild.start(); // iniciamos el proceso
+			estado = proceso.waitFor();// El estado espera hasta que termine el proceso por completo
+			System.out.println("El comando " + comando + " ha resultado " + estado);
+
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+		if (estado == 0) {
+			// Ha ido ok, se muestra el ficheroSalida
+			System.out.println("Consultar salida en: " + ficheroSalida);
+
+		} else {
+			// Se muestra el fichero log
+			System.out.println("Consultar log en: " + ficheroSalidaError);
+
+		}
+
+	}
+
+}
